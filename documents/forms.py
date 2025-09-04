ccc82aedbe39bbffe6ba0333bbe8ec748c286afa
kevinENP <kevin109235236@gmail.com>
2025-09-04 02:52:28 -0500

    algunos arreglos y ajustes en la vista de asesora

--- documents/forms.py
+++ documents/forms.py
@@ -9,3 +9,20 @@ class ContratoUploadForm(forms.ModelForm):
             "fecha_inicio": forms.DateInput(attrs={"type":"date"}),
             "fecha_fin": forms.DateInput(attrs={"type":"date"}),
         }
+
+class ContratoModalForm(forms.ModelForm):
+    class Meta:
+        model = Contrato
+        fields = ["numero_contrato", "fecha_generacion", "fecha_inicio", "valor_pago", "archivo"]
+        widgets = {
+            "fecha_generacion": forms.DateInput(attrs={"type": "date"}),
+            "fecha_inicio": forms.DateInput(attrs={"type": "date"}),
+        }
+
+    def clean_archivo(self):
+        f = self.cleaned_data.get("archivo")
+        if f:
+            fname = f.name.lower()
+            if not (fname.endswith(".pdf")):
+                raise forms.ValidationError("Solo se permiten archivos PDF.")
+        return f
