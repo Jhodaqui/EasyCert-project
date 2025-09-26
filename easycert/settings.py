ebc8ef5410d92dc21cad6c6e34f253afaed2a205
samusolis <solisreyes2005@gmail.com>
2025-09-26 07:48:18 -0500

    aun instalando tailwind

--- easycert/settings.py
+++ easycert/settings.py
@@ -46,9 +46,13 @@ INSTALLED_APPS = [
     'users',
     'certificates',
     'documents',
-   
-    
+    "tailwind",
+    "theme",              
+    "django_browser_reload"
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'easycert.urls'
